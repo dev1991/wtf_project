d92c00df08c99eaa41ed86dcb17ce7ccd82e38f0
Ishu Madan <dev@waytofun.com>
2014-01-13 10:53:54 -0800

    to email change

--- app/Controller/SubscribersController.php
+++ app/Controller/SubscribersController.php
@@ -3,7 +3,6 @@ class SubscribersController extends AppController {
     public function launching_soon_home() {
         $this->layout='launching_soon';
         if ($this->request->is('post')) {
-            // $this->Subscriber->set($this->request->data);
             $to = $this->request->data['Subscriber']['email'];
             $subject = "Subscription successful";
             $message = "Thanks, you have successfully subscribed to updates and notifications from WayToFun";
@@ -11,7 +10,7 @@ class SubscribersController extends AppController {
             $mailSent = mail($to, $subject, $message, $header);
             $this->request->data[] = $mailSent;
             if($mailSent) {
-                mail("ishu.madan@waytofun.com, ishu.madan91@gmail.com","Email Subscription", "The following addess has been subscribed to  WayToFun updates: ".$to, $header );
+                mail("ishu.madan91@gmail.com, harneh18@gmail.com","Email Subscription", "The following addess has been subscribed to  WayToFun updates: ".$to, $header );
                 $this->Subscriber->create();
                 if ($this->Subscriber->save($this->request->data)) {
                     return;
