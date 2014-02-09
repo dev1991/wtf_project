7459845af094c2328cf7ec15530f1acfb7a10866
Ishu Madan <dev@waytofun.com>
2014-02-09 12:08:28 -0800

    holiday form addition of pages

--- holiday_request_form.php
+++ holiday_request_form.php
@@ -9,44 +9,74 @@
             </ul>
             <form class="holiday_form">
                 <h4>We provide <strong>best quality</strong>, <strong>best price</strong> and <strong>100% customer satisfaction</strong>. This is the only thing we do. Rest you have to do. <strong>What?</strong> To have fun on your holiday :p .</h4>
-                <div class="holiday_form_fields_wrap">
-                    <h4>Trip Details</h4>
-                    <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Going To">
-                    </div>
-                    <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Leaving From">
-                    </div>
-                    <div class="form-group">
-                        <input type="text" class="form-control date js-date" placeholder="Departure date">
-                    </div>
-                    <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Duration">
-                        <!-- <div class="btn-group">
-                            <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">Action <span class="caret"></span>
-                            </button>
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="#">Action</a></li>
-                                <li><a href="#">Another action</a></li>
-                                <li><a href="#">Something else here</a></li>
-                                <li class="divider"></li>
-                                <li><a href="#">Separated link</a></li>
-                            </ul>
-                        </div> -->
-                    </div>
-                    <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Rooms">
-                    </div>
-                    <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Adults (age 12+)">
-                    </div>
-                    <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Childer (age 2-11)">
-                    </div>
-                    <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Infants (age 0-2)">
-                    </div>
+                <div class="cycle-slideshow holiday_form_slider" data-cycle-fx="scrollHorz" data-cycle-timeout="0" data-cycle-slides="section" data-cycle-swipe="true" data-cycle-allow-wrap="false" data-cycle-log="false" data-cycle-prev="#prev" data-cycle-next="#next">
+                    <section>
+                        <h4 class="holiday_form_group_head">Trip Details</h4>
+                        <div class="holiday_form_fields_wrap clearfix">
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Going To">
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Leaving From">
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control date js-date" placeholder="Departure date">
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Duration">
+                                <!-- <div class="btn-group">
+                                    <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">Action <span class="caret"></span>
+                                    </button>
+                                    <ul class="dropdown-menu" role="menu">
+                                        <li><a href="#">Action</a></li>
+                                        <li><a href="#">Another action</a></li>
+                                        <li><a href="#">Something else here</a></li>
+                                        <li class="divider"></li>
+                                        <li><a href="#">Separated link</a></li>
+                                    </ul>
+                                </div> -->
+                            </div>
+                        </div>
+                        <h4 class="holiday_form_group_head" >Number of persons travelling</h4>
+                        <div class="holiday_form_fields_wrap three_fields clearfix">
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Adults (age 12+)">
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Childer (age 2-11)">
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Infants (age 0-2)">
+                            </div>
+                        </div>
+                    </section>
+                    <section>
+                        <h4 class="holiday_form_group_head">Hotel Accommodation</h4>
+                    </section>
+                    <section>
+                        <h4 class="holiday_form_group_head">Contact Information</h4>
+                        <div class="holiday_form_fields_wrap clearfix">
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Name">
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Contact Number">
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Email ID">
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Country">
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="City">
+                            </div>
+                        </div>
+                    </section>
                 </div>
+                <div class="cycle-prev icon-arrow-left" id="prev"></div>
+                <div class="cycle-next icon-arrow-right" id="next"></div>
+                <button class="btn-primary" type="submit">Plan My Holidays/Post A Request</button>
             </form>
         </div>
     </div>
