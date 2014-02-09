bf4f128273850a07c87607be76d2555618d236b5
Ishu Madan <dev@waytofun.com>
2014-02-09 08:59:45 -0800

    structure changes

--- holiday_request_form.php
+++ holiday_request_form.php
@@ -1,63 +1,52 @@
 <?php include 'includes/header_launching_soon.html'; ?>
-        <div class="launching_soon_container">
-            <div class="launching_soon_content">
-                <h2>You are just three steps away to enjoy your best holiday ever...</h2>
-                <ul>
-                    <li><p>1. Choose your travel destination from our website.</p></li>
-                    <li><p>2. Place a request by filling this form. Tell us where you want to go.</p></li>
-                    <li><p>3. Now sit and relax, we will get back to you with amazing deals.</p></li>
-                </ul>
-                <form class="holiday_form">
-                    <h4>We provide <strong>best quality</strong>, <strong>best price</strong> and <strong>100% customer satisfaction</strong>. This is the only thing we do. Rest you have to do. <strong>What?</strong> To have fun on your holiday :p .</h4>
-                    <div class="holiday_form_fields_wrap">
-                        <div class="form-group">
-                            <input type="text" class="form-control" placeholder="Going To">
-                        </div>
-                        <div class="form-group">
-                            <input type="text" class="form-control" placeholder="Leaving From">
-                        </div>
-                        <div class="form-group">
-                            <input type="text" class="form-control date js-date" placeholder="Departure date">
-                        </div>
-                        <div class="form-group">
-                            <input type="text" class="form-control date" placeholder="Duration">
-                            <!-- <div class="btn-group">
-                                <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">Action <span class="caret"></span>
-                                </button>
-                                <ul class="dropdown-menu" role="menu">
-                                    <li><a href="#">Action</a></li>
-                                    <li><a href="#">Another action</a></li>
-                                    <li><a href="#">Something else here</a></li>
-                                    <li class="divider"></li>
-                                    <li><a href="#">Separated link</a></li>
-                                </ul>
-                            </div> -->
-                        </div>
-                        <div class="form-group">
-                            <input type="text" class="form-control date" placeholder="Rooms">
-                        </div>
-                        <div class="form-group">
-                            <input type="text" class="form-control date" placeholder="Adults (age 12+)">
-                        </div>
-                        <div class="form-group">
-                            <input type="text" class="form-control date" placeholder="Childer (age 2-11)">
-                        </div>
-                        <div class="form-group">
-                            <input type="text" class="form-control date" placeholder="Infants (age 0-2)">
-                        </div>
+    <div class="launching_soon_container">
+        <div class="launching_soon_content">
+            <h2>You are just three steps away to enjoy your best holiday ever...</h2>
+            <ul>
+                <li><p>1. Choose your travel destination from our website.</p></li>
+                <li><p>2. Place a request by filling this form. Tell us where you want to go.</p></li>
+                <li><p>3. Now sit and relax, we will get back to you with amazing deals.</p></li>
+            </ul>
+            <form class="holiday_form">
+                <h4>We provide <strong>best quality</strong>, <strong>best price</strong> and <strong>100% customer satisfaction</strong>. This is the only thing we do. Rest you have to do. <strong>What?</strong> To have fun on your holiday :p .</h4>
+                <div class="holiday_form_fields_wrap">
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Going To">
                     </div>
-                </form>
-            </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Leaving From">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control date js-date" placeholder="Departure date">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control date" placeholder="Duration">
+                        <!-- <div class="btn-group">
+                            <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">Action <span class="caret"></span>
+                            </button>
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="#">Action</a></li>
+                                <li><a href="#">Another action</a></li>
+                                <li><a href="#">Something else here</a></li>
+                                <li class="divider"></li>
+                                <li><a href="#">Separated link</a></li>
+                            </ul>
+                        </div> -->
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control date" placeholder="Rooms">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control date" placeholder="Adults (age 12+)">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control date" placeholder="Childer (age 2-11)">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control date" placeholder="Infants (age 0-2)">
+                    </div>
+                </div>
+            </form>
         </div>
-        <ul class="social_tabs_container">
-            <li><a href="#" class="icon-facebook"><span>Facebook</span></a></li>
-            <li><a href="#" class="icon-twitter"><span>Twitter</span></a></li>
-            <li><a href="#" class="icon-linkedin"><span>Linkedin</span></a></li>
-        </ul>
-        <div class="cycle-slideshow launching_soon_slider_wrap" data-cycle-fx="fade" data-cycle-timeout="4000" data-cycle-slides=".background_slides" data-cycle-swipe="true" data-cycle-allow-wrap="true" data-cycle-log="false">
-            <div class="background_slides" style="background:url('images/slider_img1.jpg');background-size:cover;"></div>
-            <div class="background_slides" style="background:url('images/slider_img2.jpg');background-size:cover;"></div>
-            <div class="background_slides" style="background:url('images/slider_img3.jpg');background-size:cover;"></div>
-        </div>
-    </body>
-</html>
+    </div>
+<?php include 'includes/footer_launching_soon.html'; ?>
